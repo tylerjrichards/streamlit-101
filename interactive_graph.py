ec952530dc565aca000e67af5a4a32ad5e059cf9
tylerjrichards <tylerjrichards@gmail.com>
2022-09-29 12:16:40 -0400

    finalized the interactive_graph

--- interactive_graph.py
+++ interactive_graph.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-n_rand = st.slider("Select Number of Random Numbers to Generate", min_value=1)
+st.title("Interactive Random Number Generator")
+n_rand = st.slider("Select Number of Random Numbers to Generate", min_value=5, value=50)
 chart_data = pd.DataFrame(np.random.randn(n_rand, 3), columns=["a", "b", "c"])
-st.write(chart_data)
 st.line_chart(chart_data)
