91a15f4e7e7bf900c074ae2211197e50f460b786
tylerjrichards <tylerjrichards@gmail.com>
2022-12-19 16:39:27 -0800

    added dist selection

--- interactive_graph.py
+++ interactive_graph.py
@@ -5,10 +5,25 @@ import streamlit as st
 
 st.title("Interactive Random Number Generator")
 n_rand = st.slider("Select Number of Random Numbers to Generate", min_value=5, value=50)
-chart_data = pd.DataFrame(np.random.randn(n_rand, 3), columns=["a", "b", "c"])
-line_chart = (
-    alt.Chart(chart_data).mark_line().encode(x="a", y="b", color=alt.value("red"))
+dist_selection = st.selectbox(
+    label="Select Your Distribution",
+    options=["Normal Distribution", "Uniform Distribution"],
 )
-st.altair_chart(line_chart)
+if dist_selection == "Normal Distribution":
+    chart_data = pd.DataFrame(np.random.randn(n_rand, 3), columns=["a", "b", "c"])
+else:
+    chart_data = pd.DataFrame(np.random.rand(n_rand, 3), columns=["a", "b", "c"])
 
-st.line_chart(chart_data)
+binned_data = pd.cut(chart_data["a"], bins=[-3, -2, -1, 0, 1, 2, 3])
+st.write(chart_data["a"])
+st.write(binned_data)
+st.write(chart_data)
+altair_chart = (
+    alt.Chart(chart_data)
+    .mark_bar()
+    .encode(
+        alt.X("a", bin=True),
+        y="count()",
+    )
+)
+st.altair_chart(altair_chart)
